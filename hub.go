538b929893d1d12dc71327f845d4d88a7db89c56
oseayemenre <oseayemenre04@gmail.com>
2024-12-28 14:50:01 +0100

    .

--- hub.go
+++ hub.go
@@ -1,12 +1,16 @@
 package main
 
-import "log/slog"
+import (
+	"log/slog"
+	"sync"
+)
 
 type hub struct {
 	clients    map[string]*client
 	register   chan *client
 	unregister chan *client
 	broadcast  chan []byte
+	mu         *sync.Mutex
 }
 
 func NewHub() *hub {
@@ -15,6 +19,7 @@ func NewHub() *hub {
 		register:   make(chan *client),
 		unregister: make(chan *client),
 		broadcast:  make(chan []byte),
+		mu:         &sync.Mutex{},
 	}
 }
 
@@ -22,11 +27,15 @@ func (h *hub) run() {
 	for {
 		select {
 		case client := <-h.register:
+			h.mu.Lock()
 			h.clients[client.id] = client
+			h.mu.Unlock()
 			slog.Info("Client registered", slog.String("id", client.id))
 		case client := <-h.unregister:
 			client.conn.Close()
+			h.mu.Lock()
 			delete(h.clients, client.id)
+			h.mu.Unlock()
 			slog.Info("Client unregistered", slog.String("id", client.id))
 		case message := <-h.broadcast:
 			for id := range h.clients {
